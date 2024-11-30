f252e1fe174be10e4c2f6106e23e87c8e5c9af12
Konstantin <konstantin.muehlek@gmail.com>
2024-11-30 21:56:01 +0100

    Vorbretiungen für Aufgabe 1 Abgeschlossen

---
+++ 2024/Aufgabe 1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+int main() {
+    
+    int sum = 0; 
+
+    sum += 4; 
+
+    std::cout << "Test" << sum << std::endl;
+
+  return 0;
+}
